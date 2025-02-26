ba333af65dce2439aad4681878198b82dcff9f15
CptMacTavish <stas75111@gmail.com>
2025-02-26 16:39:17 +0200

    fixed name

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header () {
     return <nav className="header">
         <div className="nav-wrapper">
-        <a href="www.youtube.com" className="brand-logo">React Movies App</a>
+        <a href="www.youtube.com" className="brand-logo">React Movies</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a href="!#">Repo</a></li>
         </ul>
